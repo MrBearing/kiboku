0ac5c9cca2e3c70dd0934971774d447a384fa4f8
MrBearing <takumi1988okamoto@gmail.com>
2026-04-01 12:44:16 +0900

    feat: extend CMakeInfo with targets and link data

--- src/models/cmake.rs
+++ src/models/cmake.rs
@@ -8,8 +8,24 @@ pub struct FindPackage {
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct TargetDependencies {
+    pub target: String,
+    pub dependencies: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct TargetLinks {
+    pub target: String,
+    pub libraries: Vec<String>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct CMakeInfo {
     pub find_packages: Vec<FindPackage>,
     pub has_catkin_package: bool,
     pub has_ament_package: bool,
+    pub executables: Vec<String>,
+    pub libraries: Vec<String>,
+    pub ament_target_dependencies: Vec<TargetDependencies>,
+    pub target_link_libraries: Vec<TargetLinks>,
 }
